a9c191dea76d35763de175cf48550da70450d4e5
JubyAnn <jubyjohnann@gmail.com>
2020-04-08 16:55:26 -0400

    Initial commit

---
+++ JavaBasics/src/MiscNotes.java
@@ -0,0 +1,20 @@
+
+public class MiscNotes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		/** True executes the first string and false executes the second string **/
+		/*String result = false ? "I am true":"I am false";
+		System.out.println(result);*/
+		
+		/** Below give if statements are correct and work fine **/
+		 /* if(true) ;
+		if(true) {}
+		if(true) {;}
+		if(true) {;;}
+		if(true) ;{};*/
+
+	}
+
+}
