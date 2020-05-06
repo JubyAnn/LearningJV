0fb275ead6bc096f7d7ecf6d5e93dd5c44fe8fcb
JubyAnn <jubyjohnann@gmail.com>
2020-05-05 23:08:40 -0400

    String Updates 05/05/20

--- JavaBasics/src/differentTopics/package-info.java
+++ JavaBasics/src/differentTopics/package-info.java
@@ -3,6 +3,14 @@
  */
 /**
  * @author jjohn
+ * 
+ * To push changes from eclipse to github
+ * 
+ * git status - tells all changes
+ * git add . - To add all files
+ * git commit -m "comments"
+ * git push origin master
+ * 
  *
  */
 package differentTopics;
--- JavaBasics/src/string/StringMethods.java
+++ JavaBasics/src/string/StringMethods.java
@@ -1,47 +1,88 @@
 package string;
 
-/*
- * toString() is an inbuilt method in Java which is used to return the String representation of an Object
- * All Java class extend Object class by default therefore every class inherits this method when creating a class it 
- * is best practice to override this method.
- *  Parameters: The method does not accept any parameters. 
- * Return Value:The method returns the string object of the particular Integer value.
- */
-
 public class StringMethods {
 	
-	int roll;
-	String name;
+	String n ;
 	
-	StringMethods(int roll, String name)
+	/**find indexOf*ß*/
+	//try this//public  void indexMethod(String str,char a)
+	public  int indexMethod(String str,char a)
 	{
-		this.roll = roll;
-		this.name = name;
-	}
-	
-	public String toString()  /*Remove this and then run code first it will print object value*/
-	{
-		return roll + " " +name;
+	 //try this//System.out.println(str.indexOf(a));
+		return str.indexOf(a);
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+	
+				/**** USING charAt() *****/
+		String pirateMessage = "  BurieD TreasurE ChesT! ";
+		String s ;
 		
-		StringMethods sm = new StringMethods(10,"Shweta");
-		StringMethods sm1 = new StringMethods(20,"Tina");
+		StringMethods sm = new StringMethods();
 		
-		//System.out.println(sm.name + " "+ sm.roll);
-		//System.out.println(sm1.name + " "+ sm1.roll);
-		
-		//System.out.println(sm); 
-		/****prints the hashcode values of the objects 
-		but we want to print the values of these objects. 
-		 * Since java compiler internally calls toString() method, 
-		overriding this method will return the specified values.*** hence created a toString method in class*/
-		
-		System.out.println(sm);
-		System.out.println(sm1.toString());
-		
+		for(int i=0; i<=24 ; i++)
+		{
+			System.out.print(pirateMessage.charAt(i));
 		}
+		System.out.println("");
+		
+		/**find indexOf**/
+		//try this //sm.indexMethod("abcdefghssfsfsf", 'f')
+		//try this /sm.indexMethod("abcdefghssfsfsf", 'f');
+		System.out.println(sm.indexMethod("abcdefghssfsfsf", 'f'));
+		
+		/**find length**/
+		System.out.println(pirateMessage.length());
+		
+		//Note: when array reference an instance variable in their state we use ".lenght" without brackets
+		
+		/**Concat is used to add a String at the end of another String. Append adds a String or 
+		 * character sequence to StringBufffer. Concat creates a new String object, whereas StringBuffer append doesn't. **/
+		
+		System.out.println(pirateMessage.concat("Can to print this !!!"));
+			
+		s = pirateMessage.concat("This is learning phase !!!");
 
-}
+		System.out.println(s);
+		
+		/** Replace Method**/
+		
+		System.out.println(pirateMessage.replace("r","S"));
+		System.out.println(pirateMessage.replace("Chest","Coins"));
+		
+		/** startsWith returns a boolena value**/
+		System.out.println(pirateMessage.startsWith("C",18));
+		System.out.println(pirateMessage.startsWith("  Buried"));
+		
+		/** endsWith returns boolean value **/
+		System.out.println(pirateMessage.endsWith("Chest! "));
+		System.out.println(pirateMessage.endsWith(" "));
+		
+		/** substring **/
+		
+		System.out.println(pirateMessage.substring(2,7));
+		System.out.println(pirateMessage.substring(5));
+		
+		/** Trim Method returns entire string minus leading & trailing whitespace **/
+		System.out.println(pirateMessage.trim());
+		
+		/** toLowerCase returns the entire string as lower case **/
+		System.out.println(pirateMessage.toLowerCase());
+		System.out.println(pirateMessage.substring(6).toLowerCase().trim());
+		
+		/** toUpperCase returns the entire string as upper case **/
+		System.out.println(pirateMessage.toUpperCase());
+		System.out.println(pirateMessage.substring(6).toUpperCase().trim());
+		
+		/** equalsIgnoreCase returns boolean value after comparing two strings while ignoring case consideration **/
+		
+		System.out.println(pirateMessage.equalsIgnoreCase("  BurieD treasurE ChesT! "));
+		String pm = "  BurieD treasurE hesT! ";
+		System.out.println(pirateMessage.equalsIgnoreCase(pm));
+	}
+
+	
+	}
+
+
